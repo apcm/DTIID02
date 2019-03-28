40dd8cb257940ba692a3bac44b80eeff7f748715
juanant1998 <juanant98@gmail.com>
2019-03-28 16:50:12 +0100

    Signed-off-by: juanant1998 <juanant98@gmail.com>

--- FIXEOS/Acme-Parade/src/main/java/domain/Chapter.java
+++ FIXEOS/Acme-Parade/src/main/java/domain/Chapter.java
@@ -4,12 +4,17 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "area")
+})
 public class Chapter extends Actor {
 
 	private String	title;
